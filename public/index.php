ac8496b4b4a6e1abdd9e8c70515650377fdd517c
Alan Arango <allaramo@gmail.com>
2020-05-10 19:59:31 +0100

    initial php structure

---
+++ public/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alan Arango</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">  
+</head>
+<body>
+
+    <div id="bg-container">
+        <div id="main-container">
+            <!-- 
+            Navbar code has been sourced from
+            https://getbootstrap.com/docs/4.4/components/navbar/ 
+            and adapted with code sourced from
+            https://www.codeply.com/go/qhaBrcWp3v        
+            -->           
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="education.html">Education</a>
+                        </li>                      
+                    </ul>
+                </div>
+                <div class="mx-auto order-0">
+                    <a class="navbar-brand mx-auto" href="#">Alan Arango</a>                  
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="skills.html">Skills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="work_experience.html">Work Experience</a>
+                        </li>
+                    </ul>
+                </div>
+          </nav>
+          <!--End of Navbar-->  
+          
+          Alan Arango
+        </div>
+    </div>
+
+    <!--Including javascripts for bootstrap-->
+    <script src="js/jquery-3.4.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+</body>
+</html>
